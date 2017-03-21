df6a464f46edb72d31b2b928a5a1f31ef1a2c596
Dan <dparks715@yahoo.com>
2017-03-21 19:00:16 -0400

    Setting more routes up, need to start working on functions to guess letters

--- app.rb
+++ app.rb
@@ -17,3 +17,10 @@ post '/players' do
 	erb :get_word, :locals => {p1: session[:p1], p2: session[:p2]}
 
 end
+
+post '/game' do
+	session[:guess_word] = params[:guess_word]
+
+	erb :main_game :locals => {p1: session[:p1], p2: session[:p2], guess_word: session[:guess_word]}
+
+end
